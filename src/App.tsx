15ac4b4e7bfcf9262f86904c13e7e348c9904fa7
Riku <rikumomo1004@gmail.com>
2025-05-19 19:17:49 +0900

    Headerをコンポーネント化した

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import {
 } from "@mui/material";
 import "./App.css";
 import { useState } from "react";
+import { Header } from "./Header";
 
 function App() {
   const [todoList, setTodoList] = useState<string[]>([]);
@@ -23,23 +24,7 @@ function App() {
   };
   return (
     <>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            ></IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              News
-            </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
-      </Box>
+      <Header />
       <Box display={"flex"} flexDirection={"column"} alignItems={"center"}>
         <Box display={"flex"} alignItems={"center"} my={6}>
           <TextField
