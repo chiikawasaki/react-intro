5b7761014c8b04f5e519db31def98e255fdd4fed
ryugoshimizu <ryugo.smz@icloud.com>
2025-05-19 16:40:54 +0900

    todolist追加

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,21 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 
 import "./App.css";
-import { FormGroup, FormControlLabel, Checkbox } from "@mui/material";
+import {
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
+  TextField,
+} from "@mui/material";
+import { useState } from "react";
 
 function App() {
+  const [todoList, setTodoList] = useState<string[]>([]);
+  const [newTodo, setNewTodo] = useState("");
+  const handleAddTodo = () => {
+    setTodoList([...todoList, newTodo]);
+    setNewTodo("");
+  };
   return (
     <Box>
       <Box sx={{ flexGrow: 1 }}>
@@ -21,14 +33,32 @@ function App() {
         </AppBar>
       </Box>
 
-      <Box display={"flex"} justifyContent={"center"}>
+      <Box
+        display={"flex"}
+        flexDirection={"column"}
+        alignItems={"center"}
+        marginTop={6}
+      >
+        <Box my={6}>
+          <TextField
+            id="standard-basic"
+            label="ToDo入力"
+            variant="standard"
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+          />
+          <Button variant="contained" onClick={handleAddTodo}>
+            追加
+          </Button>
+        </Box>
         <FormGroup>
+          {todoList.map((todo, index) => (
+            <FormControlLabel key={index} control={<Checkbox />} label={todo} />
+          ))}
           <FormControlLabel
             control={<Checkbox defaultChecked />}
             label="todo"
           />
-          <FormControlLabel required control={<Checkbox />} label="Required" />
-          <FormControlLabel disabled control={<Checkbox />} label="Disabled" />
         </FormGroup>
       </Box>
     </Box>
