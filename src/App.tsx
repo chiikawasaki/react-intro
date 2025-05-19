342b9c88bb52d127879b0346d2d08e37636b6801
Riku <rikumomo1004@gmail.com>
2025-05-19 19:01:12 +0900

    To Do リストを作成した

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,64 @@
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  TextField,
+} from "@mui/material";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return <h1>Hello World</h1>;
-}
+  const [todoList, setTodoList] = useState<string[]>([]);
+  const [newTodo, setNewTodo] = useState("");
 
+  const handleAddTodo = () => {
+    setTodoList([...todoList, newTodo]);
+    setNewTodo("");
+  };
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            ></IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Box display={"flex"} flexDirection={"column"} alignItems={"center"}>
+        <Box display={"flex"} alignItems={"center"} my={6}>
+          <TextField
+            id="standard-basic"
+            label="To Do"
+            variant={"standard"}
+            onChange={(e) => setNewTodo(e.target.value)}
+          />
+          <Button variant="contained" onClick={handleAddTodo}>
+            追加
+          </Button>
+        </Box>
+        {todoList.map((todo, index) => (
+          <FormGroup>
+            <FormControlLabel control={<Checkbox />} label={todo} key={index} />
+          </FormGroup>
+        ))}
+      </Box>
+    </>
+  );
+}
 export default App;
