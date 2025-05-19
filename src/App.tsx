52c2aaa6361f077b1f7761f10da3226716e20d69
ryugoshimizu <ryugo.smz@icloud.com>
2025-05-19 15:52:52 +0900

    チェックボックスとヘッダーを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
 import "./App.css";
+import { FormGroup, FormControlLabel, Checkbox } from "@mui/material";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Box>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+
+      <Box display={"flex"} justifyContent={"center"}>
+        <FormGroup>
+          <FormControlLabel
+            control={<Checkbox defaultChecked />}
+            label="todo"
+          />
+          <FormControlLabel required control={<Checkbox />} label="Required" />
+          <FormControlLabel disabled control={<Checkbox />} label="Disabled" />
+        </FormGroup>
+      </Box>
+    </Box>
+  );
 }
 
 export default App;
