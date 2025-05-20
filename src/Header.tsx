d7eb6be4c92572e04399ce13f2d6ab88407eb5f0
AOi126 <yamaa439966@gmail.com>
2025-05-20 14:42:55 +0900

    Headerをコンポーネント化した

---
+++ src/Header.tsx
@@ -0,0 +1,18 @@
+import { Box, AppBar, Toolbar, Typography, Button } from "@mui/material";
+
+const Header = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default Header;
