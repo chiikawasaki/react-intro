26639804c3096a8e308b152ae5809a4bd4fe7225
AOi126 <yamaa439966@gmail.com>
2025-05-20 14:42:01 +0900

    Todoリスト作った

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,49 @@
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  TextField,
+} from "@mui/material";
 import "./App.css";
+import { useState } from "react";
+import Header from "./Header";
 
 function App() {
-  return <h1>Hello World</h1>;
+  const [todoList, setTodoList] = useState<string[]>([]);
+  const [newTodo, setNewTodo] = useState<string>("");
+
+  const handleAddTodo = () => {
+    setTodoList([...todoList, newTodo]);
+    setNewTodo("");
+  };
+
+  return (
+    <>
+      <Header />
+      <Box display={"flex"} alignItems={"center"} flexDirection={"column"}>
+        <Box display={"flex"} alignItems={"center"} my={6}>
+          <TextField
+            id="standard-basic"
+            label="Todo"
+            variant="standard"
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+          />
+          <Button variant="contained" onClick={handleAddTodo}>
+            追加
+          </Button>
+        </Box>
+        {todoList.map((todo, index) => (
+          <FormControlLabel control={<Checkbox />} label={todo} key={index} />
+        ))}
+      </Box>
+    </>
+  );
 }
 
 export default App;
